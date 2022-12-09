da2cda71e7d854ed5f93998067cfc300a4fd8491
ramyar omed ali <ro21090@auis.edu.krd>
2022-12-09 23:39:19 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array,word){
+    const newarray=[];
+    for(let i=0; i<array.length;i++){
+    newarray.push(`Thank you, ${array[i]}, for the wonderful ${word} gift!`)
+    }
+    return newarray;
+}
+
+function countDown(Number){
+    let i = 0;
+    while (i <= Number) {
+        console.log(i);
+        i++; 
+      }
+}
